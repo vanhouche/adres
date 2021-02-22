90c45c39dddc1980a0617d43550d7bd257e1976f
giggle <giggle@DESKTOP-94E1GTH>
2021-02-22 15:35:20 +0100

    Add project files.

---
+++ adres/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("voornaam : Sven");
+            Console.WriteLine("achternaam: Vanhouche");
+            Console.WriteLine("straat: Groenstraat");
+            Console.WriteLine("postcode: 2547");
+            Console.WriteLine("gemeente: Lint");
+            Console.WriteLine("telefoonnummer: 0493910183");
+            Console.WriteLine("emailadres: giggle@telenet.be");
+            Console.WriteLine("Naam = sven vanhouche");
+            Console.WriteLine("Adres = Groenstraat 34, 2547 Lint");
+
+            Console.ReadLine();
+        }
+    }
+}
