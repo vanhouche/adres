55f571a1206df2aea8d1a75ce1362c2ed1eae85c
giggle <giggle@DESKTOP-94E1GTH>
2021-02-22 15:45:06 +0100

    adres

--- adres/Program.cs
+++ adres/Program.cs
@@ -17,10 +17,13 @@ namespace adres
             Console.WriteLine("gemeente: Lint");
             Console.WriteLine("telefoonnummer: 0493910183");
             Console.WriteLine("emailadres: giggle@telenet.be");
+
+            Console.ReadLine();
             Console.WriteLine("Naam = sven vanhouche");
             Console.WriteLine("Adres = Groenstraat 34, 2547 Lint");
 
             Console.ReadLine();
+
         }
     }
 }
